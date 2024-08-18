cf94a61a6db19a4e6d290d0b60de10a7d088cdc9
CrimsonSpark <zandrewepener012@gmail.com>
2024-08-18 03:17:01 +0200

    Add 'ContactInquiry' model

---
+++ codecraft-web/Models/ContactInquiry.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace codecraft_web.Models
+{
+    public class ContactInquiry
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, DataType(DataType.EmailAddress), MaxLength(320)]
+        public string Email { get; set; }
+        [Required]
+        public string Message { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedAt { get; set; } // TIMESTAMP
+    }
+}
