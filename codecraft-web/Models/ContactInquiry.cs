a3cf00815cbd97f0ef74a50b4902ce5934a7f977
CrimsonSpark <zandrewepener012@gmail.com>
2024-08-18 04:16:12 +0200

    Update 'ContactInquiry' model

--- codecraft-web/Models/ContactInquiry.cs
+++ codecraft-web/Models/ContactInquiry.cs
@@ -6,12 +6,12 @@ namespace codecraft_web.Models
     public class ContactInquiry
     {
         [Key]
-        public int Id { get; set; }
+        public long Id { get; set; }
         [Required, DataType(DataType.EmailAddress), MaxLength(320)]
         public string Email { get; set; }
         [Required]
         public string Message { get; set; }
         [DataType(DataType.DateTime)]
-        public DateTime CreatedAt { get; set; } // TIMESTAMP
+        public DateTime CreatedAt { get; set; }
     }
 }
