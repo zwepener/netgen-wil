cc1b9132d3a087a2809c7e8f26ff071ebf4468aa
CrimsonSpark <zandrewepener012@gmail.com>
2024-08-18 03:18:12 +0200

    Add 'CourseCategory' model

---
+++ codecraft-web/Controllers/CourseCategoriesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using codecraft_web.Data;
+using codecraft_web.Models;
+
+namespace codecraft_web.Controllers
+{
+    public class CourseCategoriesController : Controller
+    {
+        private readonly codecraft_webDBContext _context;
+
+        public CourseCategoriesController(codecraft_webDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CourseCategories
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.CourseCategory.ToListAsync());
+        }
+
+        // GET: CourseCategories/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var courseCategory = await _context.CourseCategory
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (courseCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(courseCategory);
+        }
+
+        // GET: CourseCategories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CourseCategories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,CreatedAt,UpdatedAt")] CourseCategory courseCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(courseCategory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(courseCategory);
+        }
+
+        // GET: CourseCategories/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var courseCategory = await _context.CourseCategory.FindAsync(id);
+            if (courseCategory == null)
+            {
+                return NotFound();
+            }
+            return View(courseCategory);
+        }
+
+        // POST: CourseCategories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,Name,CreatedAt,UpdatedAt")] CourseCategory courseCategory)
+        {
+            if (id != courseCategory.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(courseCategory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CourseCategoryExists(courseCategory.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(courseCategory);
+        }
+
+        // GET: CourseCategories/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var courseCategory = await _context.CourseCategory
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (courseCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(courseCategory);
+        }
+
+        // POST: CourseCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var courseCategory = await _context.CourseCategory.FindAsync(id);
+            if (courseCategory != null)
+            {
+                _context.CourseCategory.Remove(courseCategory);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CourseCategoryExists(long id)
+        {
+            return _context.CourseCategory.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ codecraft-web/Models/CourseCategory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace codecraft_web.Models
+{
+    public class CourseCategory
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(32)]
+        public string Name { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedAt { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
