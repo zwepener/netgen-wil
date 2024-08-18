d69bcdb6ea9c0ae07354ffcba2954eaa739dd574
CrimsonSpark <zandrewepener012@gmail.com>
2024-08-18 03:19:52 +0200

    Add 'InstructorTestimonial' model

---
+++ codecraft-web/Models/InstructorTestimonial.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace codecraft_web.Models
+{
+    public class InstructorTestimonial
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey(nameof(Instructor))]
+        public long InstructorId { get; set; }
+        [ForeignKey(nameof(Student))]
+        public long StudentId { get; set; }
+        [Required, MaxLength(255)]
+        public string Comment { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedAt { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
