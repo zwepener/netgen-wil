7204e5138f91f4b8843951296c5e67b63459ce02
CrimsonSpark <zandrewepener012@gmail.com>
2024-08-18 03:19:00 +0200

    Add 'Enrollment' model

---
+++ codecraft-web/Models/Enrollment.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace codecraft_web.Models
+{
+    public class Enrollment
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey(nameof(Student))]
+        public long StudentId { get; set; }
+        [ForeignKey(nameof(Course))]
+        public long CourseId { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedAt { get; set; }
+    }
+}
