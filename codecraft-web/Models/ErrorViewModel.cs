e0058b330951bbe06b04ba986e5692dc6421cfa6
Zandré Wepener <zandrewepener012@gmail.com>
2024-10-10 11:59:26 +0200

    Remove more obsolete files

--- codecraft-web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace codecraft_web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
