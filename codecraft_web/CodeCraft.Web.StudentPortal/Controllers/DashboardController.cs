5847132283235c1342716b7ec207acf1d5fb6b11
Zandré Wepener <zandrewepener012@gmail.com>
2024-11-17 22:39:19 +0200

    Start working on Dashboard's controller

--- codecraft_web/CodeCraft.Web.StudentPortal/Controllers/DashboardController.cs
+++ codecraft_web/CodeCraft.Web.StudentPortal/Controllers/DashboardController.cs
@@ -1,14 +1,33 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CodeCraft.Data;
+using CodeCraft.Data.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CodeCraft.Web.StudentPortal.Controllers
 {
     [Authorize]
-    public class DashboardController : Controller
+    public class DashboardController(UserManager<IdentityUser> userManager, CodeCraftDbContext context) : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<IdentityUser> _userManager = userManager;
+        private readonly CodeCraftDbContext _context = context;
+
+        public async Task<IActionResult> Index()
         {
-            return View();
+            IdentityUser? user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            Student? student = await _context.Student.FirstAsync();
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
         }
     }
 }
