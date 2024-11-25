93ae76ea65c24e15de2d063f2859393070461911
Zandré Wepener <zandrewepener012@gmail.com>
2024-11-25 10:59:35 +0200

    Add 'Dashboard' page
    Added the 'Dashboard' page. This is now the default (Home) page. Also updated the main layout and the main login partial.
    
    Co-Authored-By: LightWay21 <142213536+LightWay21@users.noreply.github.com>

---
+++ codecraft_web/CodeCraft.Web.AdminPortal/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeCraft.Web.AdminPortal.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
