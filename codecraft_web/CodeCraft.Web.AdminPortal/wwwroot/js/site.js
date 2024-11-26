91b867fa1c084d5d9c1cd31416cd319849f05b3f
Zandré Wepener <zandrewepener012@gmail.com>
2024-11-26 22:24:19 +0200

    Update root site.js
    Added some options for the "datepicker" html class.

--- codecraft_web/CodeCraft.Web.AdminPortal/wwwroot/js/site.js
+++ codecraft_web/CodeCraft.Web.AdminPortal/wwwroot/js/site.js
@@ -1,4 +1,8 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(function () {
+    $('.datepicker').pickadate({
+        selectMonths: true,
+        selectYears: true,
+        format: 'dd/mm/yyyy',
+        formatSubmit: 'dd/mm/yyyy'
+    })
+});
