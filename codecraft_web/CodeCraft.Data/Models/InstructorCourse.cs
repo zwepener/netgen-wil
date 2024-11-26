6da75f63b6ac5824ec816b00d50cd8ab5231b32d
Zandré Wepener <zandrewepener012@gmail.com>
2024-11-26 23:04:50 +0200

    Create 'InstructorCourse' model
    This model represents the many-to-many relationships between the 'Instructor' and 'Course' database tables.

---
+++ codecraft_web/CodeCraft.Data/Models/InstructorCourse.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeCraft.Data.Models
+{
+    public class InstructorCourse
+    {
+        [Required]
+        public int CourseId { get; set; }
+        public Course? Course { get; set; }
+        [Required]
+        public int InstructorId { get; set; }
+        public Instructor? Instructor { get; set; }
+    }
+}
