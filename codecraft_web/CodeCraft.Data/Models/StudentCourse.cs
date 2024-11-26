5860f70f2ecd430231ba78e251909d848ce139f1
Zandré Wepener <zandrewepener012@gmail.com>
2024-11-26 23:11:20 +0200

    Create 'StudentCourse' model
    This model represents the many-to-many relationship between the 'Student' and 'Course' database tables.

---
+++ codecraft_web/CodeCraft.Data/Models/StudentCourse.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeCraft.Data.Models
+{
+    public class StudentCourse
+    {
+        [Required]
+        public int CourseId { get; set; }
+        public Course? Course { get; set; }
+        [Required]
+        public int StudentId { get; set; }
+        public Student? Student { get; set; }
+    }
+}
