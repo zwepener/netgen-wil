6d1f872c05f849be59298413632bfe338f891031
Zandré Wepener <zandrewepener012@gmail.com>
2024-11-27 04:08:41 +0200

    Update AdminPortal 'Dashboard/Index' view
    Added a model for the dashboard and added some data to the page.

--- codecraft_web/CodeCraft.Web.AdminPortal/Controllers/DashboardController.cs
+++ codecraft_web/CodeCraft.Web.AdminPortal/Controllers/DashboardController.cs
@@ -1,14 +1,33 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CodeCraft.Data;
+using CodeCraft.Web.AdminPortal.Models;
+using CodeCraft.Data.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CodeCraft.Web.AdminPortal.Controllers
 {
     [Authorize]
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly CodeCraftDbContext _context;
+        public DashboardController(CodeCraftDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Student> students = await _context.Student.Include(s => s.User).ToListAsync();
+            List<Course> courses = await _context.Course.ToListAsync();
+
+            return View
+            (
+                new DashboardModel
+                {
+                    Students = students,
+                    Courses = courses,
+                }
+            );
         }
     }
 }
