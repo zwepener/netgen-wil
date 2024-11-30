60ced81c569dac2424f47aa27c26fced67056c18
Zandré Wepener <zandrewepener012@gmail.com>
2024-12-01 00:27:42 +0200

    Update Admin Portal

--- codecraft_web/CodeCraft.Web.AdminPortal/Program.cs
+++ codecraft_web/CodeCraft.Web.AdminPortal/Program.cs
@@ -8,10 +8,12 @@ string? connectionString = (Environment.GetEnvironmentVariable("DefaultConnectio
 
 builder.Services.AddDbContext<CodeCraftDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<CodeCraftDbContext>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -23,16 +25,17 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.UseStatusCodePagesWithReExecute("/Error/{0}");
@@ -40,6 +43,7 @@ app.UseStatusCodePagesWithReExecute("/Error/{0}");
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+
 app.MapRazorPages();
 
 app.Run();
