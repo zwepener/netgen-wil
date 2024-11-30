e336fe49ac791f7f898c5d4b63b01915e2cd22e6
Zandré Wepener <zandrewepener012@gmail.com>
2024-11-30 23:11:57 +0200

    Update Program.cs

--- codecraft_web/CodeCraft.Web.AdminPortal/Program.cs
+++ codecraft_web/CodeCraft.Web.AdminPortal/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = (Environment.GetEnvironmentVariable("DefaultConnection") ?? builder.Configuration.GetConnectionString("DefaultConnection")) ?? throw new InvalidOperationException("Connection string not found.");
+var connectionString = (Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING") ?? builder.Configuration.GetConnectionString("DefaultConnection")) ?? throw new InvalidOperationException("Connection string not found.");
 
 builder.Services.AddDbContext<CodeCraftDbContext>(options =>
     options.UseSqlServer(connectionString));
