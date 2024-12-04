59575baf2ffee3763ef5cba61fecdac13d712f1a
Zandré Wepener <zandrewepener012@gmail.com>
2024-12-04 02:01:04 +0200

    Add Enrollment Form Modal

---
+++ codecraft_web/CodeCraft.Web.PublicPortal/Models/EnrollemntFormViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CodeCraft.Data.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeCraft.Web.PublicPortal.Models;
+
+public class EnrollemntFormViewModel
+{
+    public List<Course> Courses { get; set; } = [];
+}
