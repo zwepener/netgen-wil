16378f3e8723fadf0194ac3ac37ebb6f8587347f
Zandré Wepener <zandrewepener012@gmail.com>
2024-12-05 00:24:37 +0200

    Update Public Portal (#7)
    * Create _EnrollmentForm.cshtml
    
    i added the script as file to the project.
    
    * Update _EnrollmentFormPartial.cshtml
    
    * Add Enrollment Form Modal
    
    * Misc
    
    * Enrollment html/css
    
    ---------
    
    Co-authored-by: bryslon-vr <bryslon.vanrooyen@gmail.com>
    Co-authored-by: MZ-Bale <163714985+MZ-Bale@users.noreply.github.com>

---
+++ codecraft_web/CodeCraft.Web.PublicPortal/Models/EnrollemntFormViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeCraft.Web.PublicPortal.Models;
+
+public class EnrollemntFormViewModel
+{
+    public required int FormId { get; set; }
+    
+    public required string CourseName { get; set; }
+
+    public required int CourseId { get; set; }
+}
