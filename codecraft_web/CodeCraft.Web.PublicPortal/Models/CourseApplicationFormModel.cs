568e1b56ce907979269a037ecc64b94c454c32a4
Zandré Wepener <zandrewepener012@gmail.com>
2024-12-05 03:38:33 +0200

    [PublicPortal] Update CourseApplication form model

---
+++ codecraft_web/CodeCraft.Web.PublicPortal/Models/CourseApplicationFormModel.cs
@@ -0,0 +1,14 @@
+﻿using CodeCraft.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeCraft.Web.PublicPortal.Models;
+
+public class CourseApplicationFormModel
+{
+    public required int CourseId { get; set; }
+
+    public required string CourseName { get; set; }
+
+    [BindProperty]
+    public CourseApplication Input { get; set; } = null!;
+}
